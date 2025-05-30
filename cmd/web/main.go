14366203b0acb6808506bd9994bf86cfd6d1fca6
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2025-05-30 20:49:45 -0300

    serfing static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,6 +10,7 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./ui/static"))))
 
 	log.Println("Starting server at port: 4000")
 	err := http.ListenAndServe(":4000", mux)
