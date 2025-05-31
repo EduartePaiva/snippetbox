c2af1526d3f9271d831de7fc7f41fcb40154a150
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2025-05-30 21:15:03 -0300

    added default flag for addr

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 	mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./ui/static"))))
 
-	log.Println("Starting server at port: 4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
