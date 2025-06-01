eeec778aa5c1e8acbcfbbe4f32503c962b5cb8fb
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2025-06-01 10:37:54 -0300

    separating routes into it's own file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -14,18 +14,12 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	handler := application{infoLog, errorLog}
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", handler.home)
-	mux.HandleFunc("/snippet", handler.showSnippet)
-	mux.HandleFunc("/snippet/create", handler.createSnippet)
-	mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./ui/static"))))
+	app := application{infoLog, errorLog}
 
 	srv := http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
