b5d5eae6adca7daaee1e0784064d69ae4210c082
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2026-05-17 11:46:52 -0300

    adding dynamic current year

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,7 +5,20 @@ import (
 	"fmt"
 	"net/http"
 	"runtime/debug"
+	"time"
 )
+
+func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
+	if td == nil {
+		td = &templateData{}
+	}
+
+	year := time.Now().Year()
+
+	td.CurrentYear = year
+
+	return td
+}
 
 func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
@@ -31,7 +44,7 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 
 	buf := new(bytes.Buffer)
 
-	err := ts.Execute(buf, td)
+	err := ts.Execute(buf, app.addDefaultData(td, r))
 	if err != nil {
 		app.serverError(w, err)
 		return
