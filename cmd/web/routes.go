6c967914ef06791cdf8198553aa6b4bbc3939593
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2026-05-19 17:50:07 -0300

    creating a secureHeaders middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", app.home)
@@ -10,5 +10,5 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/snippet/create", app.createSnippet)
 	mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./ui/static"))))
 
-	return mux
+	return secureHeaders(mux)
 }
